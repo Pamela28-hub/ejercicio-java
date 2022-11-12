5ca0aa9bd196bb73a7ee44681efe291f6c74bfed
Pamela García <64838979+Pamela28-hub@users.noreply.github.com>
2022-11-12 13:38:51 +0100

    Codigo borrado
    Eliminación de cógido

--- ejercicioJava/src/ejerciciojava/EjercicioJava.java
+++ ejercicioJava/src/ejerciciojava/EjercicioJava.java
@@ -20,37 +20,7 @@ public class EjercicioJava {
         Scanner teclado = new Scanner(System.in);
         teclado.useLocale(Locale.UK);
 
-        System.out.print("Ingrese cantidad de kilómetros");
-        int kilometros = teclado.nextInt();
-        System.out.print("Tipo de billete : a)Sencillo , b)Ida y vuelta ");
-        teclado.next();
-        String tipoBillete = teclado.nextLine();
-        double precioBillete;
-       
-        if (tipoBillete.equalsIgnoreCase("a")) {
-            precioBillete = 0.33;
-            
-        } else {
-            precioBillete = 0.26;
-            
-        }
         
-        double tarifa = precioBillete*kilometros;
-        
-        
-        System.out.println("Tiene tarjeta interrail : a) si b)No");
-        String tarjetainterra = teclado.nextLine();
-        boolean sitieneIterrail = tarjetainterra.equalsIgnoreCase("a");
-        double dto = 0;
-        if (sitieneIterrail) {
-            dto = 0.25;
-            
-            
-        } else if (tarifa>110){
-            dto = 0.10;
-        }  else if (tarifa>50){
-            dto = 0.5;
-        }
         
         double tarifaTotal = tarifa - tarifa*dto;
         
